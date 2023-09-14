426b02e004b858f393c3fdd9087132890c2ab236
Hakai <hakaifire@proton.me>
2023-09-13 20:03:23 -0500

    Actulizado

---
+++ operadoresLogicos.js
@@ -0,0 +1,33 @@
+// function mayorYMenorYPar(num) 
+// {if (num > 5 && num < 10 && num % 2 ===0 )console.log(true);
+// else console.log(false); };
+
+// mayorYMenorYPar (7)
+// mayorYMenorYPar (8)
+
+// function OperadorOr(str) { 
+//     if (str ==="Henry" || str.length < 2 ) console.log (true);
+//      else console.log (false);};
+
+//      OperadorOr ("H")
+
+// function negacion(permiso){
+//     if(permiso !== true);
+//     console.log ("Tiene permiso");
+// };
+
+// negacion(false);
+
+function condicionCompleja(num) {
+    if (num > 9 && num % 2 === 0 || num === 3) 
+      console.log(true);
+     else 
+      console.log(false);
+    };
+  
+
+condicionCompleja(10);
+condicionCompleja(6);
+condicionCompleja(3);
+condicionCompleja(5);
+
