a2f8f418233cac9ce15b3037cc4d503bb47aa21c
Hakai <hakaifire@proton.me>
2023-09-23 20:29:07 -0500

    ejercicio bucles

--- bucles.js
+++ bucles.js
@@ -1,4 +1,4 @@
-var suma = 0;
+// var suma = 0;
 // suma = suma + 1;
 // suma = suma + 2;
 // suma = suma + 3;
@@ -14,7 +14,22 @@ var suma = 0;
 // console.log("Variable suma", suma);
 
 //BUCLE WHILE
-while (suma < 3){
-    suma = suma +1;
-    console.log(suma);
-}
+// while (suma < 3){
+//     suma = suma +1;
+//     console.log(suma);
+// }
+
+function doWhile(num) {
+    let contador = 0;  // 1. Inicializa una variable llamada "contador" en 0.
+    
+    do {
+      num += 5;        // 2. Aumenta el valor de "num" en 5 en cada iteración del bucle.
+      contador++;      // 3. Incrementa el contador de iteraciones en 1 en cada iteración.
+    console.log("Iteracion:", contador); console.log("Num:",num)} 
+    while (contador < 8); // 4. Ejecuta el bucle "do-while" mientras "contador" sea menor que 8.
+    
+    return num;        // 5. Retorna el valor final de "num" después de realizar las 8 iteraciones.
+  }
+  
+  // Ejemplo de uso:
+  console.log(doWhile(10)); // Devolverá 50 (10 + 5 * 8)
